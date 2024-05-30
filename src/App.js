703e630874ce06f7a839bd8931733672eff76f7a
Abhishekci <abhishekitagimath24@gmail.com>
2024-05-30 23:52:19 +0530

    React-Labs Task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Counter from './components/Counter'; // Adjust the import path according to your project structure
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import RichTextEditor from './components/RichTextEditor';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/signin' element={<SignIn/>} />
+        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/editor' element={<RichTextEditor/>} />
+        <Route path='/counter' element={<Counter/>} />
+      </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
